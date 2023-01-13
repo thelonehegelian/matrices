3890b66cd603bf657d4e8a715c4ac5f90455a477
umair iftikhar <umair.iftikhar@pulsar-systems.fi>
2023-01-13 06:16:59 +0000

    Sum arrays

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use ndarray::arr2;
+
 fn main() {
-    println!("Hello, world!");
+    // a 2d array with shape (2, 3)
+    let a = arr2(&[[1., 2., 3.], [4., 5., 6.]]);
+    let b = arr2(&[[1., 2., 3.], [4., 5., 6.]]);
+    // sum the arrays
+    let c = &a + &b;
+    println!("{:?}", c);
 }
